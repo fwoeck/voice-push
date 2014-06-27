602f9d1e6ae11d56752847c5c5d84d8af0d0229f
Frank Woeckener <fwoeck@gmail.com>
2014-06-27 11:27:56 +0200

    omit EM.synchrony

--- server.rb
+++ server.rb
@@ -18,16 +18,14 @@ require 'lib/amqp_manager'
 class Server < Goliath::API
 
   def response(env)
-    EM.synchrony do
-      @queue = AmqpManager.channel.queue(env.object_id.to_s, auto_delete: true)
-      env.logger.info "Queue #{env.object_id} opened."
+    @queue = AmqpManager.channel.queue(env.object_id.to_s, auto_delete: true)
+    env.logger.info "Queue #{env.object_id} opened."
 
-      @queue.bind(AmqpManager.xchange).subscribe do |info, meta, payload|
-        EM.next_tick do
-          env.stream_send "data:#{payload}\n\n"
-          env.logger.info "Send to #{env.object_id}: #{payload}"
-        end
-      end
+    @queue.bind(AmqpManager.xchange).subscribe do |info, meta, payload|
+      EM.next_tick {
+        env.stream_send "data:#{payload}\n\n"
+        env.logger.info "Send to #{env.object_id}: #{payload}"
+      }
     end
 
     streaming_response(200, {'Content-Type' => 'text/event-stream'})
