fb9ab583829585f8ef5ad934b374b83725ed6c4b
Frank Wöckener <fwoeck@gmail.com>
2014-08-09 10:23:10 +0200

    Squashed commit of the following:
    commit 8549ac7dc8c190890bb9726e84d983fafac84d06
    Author: Frank Wöckener <fwoeck@gmail.com>
    Date:   Sat Aug 9 09:05:34 2014 +0200
    
        use online state for agent visibility instead of registry
    
    commit 175ab1612ecaf831a9bded8b3f7f982bf73637fc
    Merge: 7074157 54fd485
    Author: Frank Wöckener <fwoeck@gmail.com>
    Date:   Wed Jul 30 11:35:50 2014 +0200
    
        Back-merged master from release #{0.0.25}
    
    commit 70741578db5a47d84bbe6de6974c0202abadf84f
    Author: Frank Wöckener <fwoeck@gmail.com>
    Date:   Wed Jul 30 10:42:47 2014 +0200
    
        update some gems
    
    commit b099b914e8a64e1175911a77f15c84ff23eb2e65
    Merge: 241794f 2bb0e69
    Author: Frank Wöckener <fwoeck@gmail.com>
    Date:   Sun Jul 20 07:45:42 2014 +0200
    
        Back-merged master from release #{0.0.23}
    
    commit 241794f641b109f5d12d561e0c1b81f3c5fb9077
    Author: Frank Wöckener <fwoeck@gmail.com>
    Date:   Sun Jul 20 07:03:17 2014 +0200
    
        change main file name, set log levels
    
    commit dda1eab16ff745a3e6c38cc0c98bcf0bb5a5d15e
    Merge: e2d3885 3ce7ae0
    Author: Frank Wöckener <fwoeck@gmail.com>
    Date:   Thu Jul 17 11:48:20 2014 +0200
    
        Back-merged master from release #{0.0.21}
    
    commit e2d38855d3724e01f97b98fb31d2345334e055b5
    Author: Frank Wöckener <fwoeck@gmail.com>
    Date:   Mon Jul 14 11:37:36 2014 +0200
    
        remove closed connections from registry
    
    commit 807f174dca2c810b8f7643a2249370d1e595015c
    Author: Frank Wöckener <fwoeck@gmail.com>
    Date:   Sun Jul 13 13:47:08 2014 +0200
    
        rescue closed sockets under jruby
    
    commit f413b2db1f2796c71b67472727aa7850cc75d334
    Merge: 90c5840 87b2c00
    Author: Frank Wöckener <fwoeck@gmail.com>
    Date:   Fri Jul 11 09:15:15 2014 +0200
    
        Merge branch 'master' into develop
    
    commit 90c58409f7e0f4fe8a68478d5baa33d963def32e
    Author: Frank Wöckener <fwoeck@gmail.com>
    Date:   Fri Jul 11 09:15:12 2014 +0200
    
        ignore merge files

--- lib/amqp_manager.rb
+++ lib/amqp_manager.rb
@@ -3,23 +3,45 @@ require 'bunny'
 module AmqpManager
   class << self
 
-    def shutdown
-      @connection.close
+
+    def ahn_channel
+      Thread.current[:ahn_channel] ||= connection.create_channel
+    end
+
+    def ahn_xchange
+      Thread.current[:ahn_xchange] ||= ahn_channel.topic('voice.ahn', auto_delete: false)
+    end
+
+    def ahn_queue
+      Thread.current[:ahn_queue] ||= ahn_channel.queue('voice.ahn', auto_delete: false)
+    end
+
+    def ahn_publish(payload)
+      ahn_xchange.publish(payload.to_json, routing_key: 'voice.ahn')
+      true
     end
 
 
     def push_channel
-      Thread.current[:push_channel] ||= @connection.create_channel
+      Thread.current[:push_channel] ||= connection.create_channel
     end
-
 
     def push_xchange
       Thread.current[:push_xchange] ||= push_channel.topic('voice.push', auto_delete: false)
     end
 
-
     def push_queue
       Thread.current[:push_queue] ||= push_channel.queue('voice.push', auto_delete: false)
+    end
+
+
+    def shutdown
+      connection.close
+    end
+
+    def connection
+      establish_connection unless @connection
+      @connection
     end
 
 
